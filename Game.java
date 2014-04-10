c2d8f9e6e78621c18f347b64da1367800f338f4e
Devin Wieker <devinwieker@Devins-MacBook-Air.local>
2014-04-09 22:24:56 -0700

    Added some notes on game logic

--- Game.java
+++ Game.java
@@ -16,18 +16,50 @@ public class Game
    //   players.add(new Player("Golf", initMoney));
    //   players.add(new Player("Hotel", initMoney));
       
-      Table t = new Table(players, smallBlind, bigBlind);   
-      
+      Table t = new Table(players, smallBlind, bigBlind); 
       t.handleBlinds();
   
       t.dealPreFlop();
-      t.dealFlop();
-  
+      
+
+      
       System.out.println("PreFlop:");
       players.get(0).getPreFlop().printHand();
-  //    System.out.println(t.firstToAct());
-  
       
+      System.out.println("First Better Position:");
+      System.out.println(t.firstToAct());
+      
+         
+         if(t.getCurrentBetter() == 0)
+         {
+            //prompt user for decision
+         }
+         else
+         {
+            //AI makes decision. t.handleDecision(AI's decision)
+         
+         }
+      
+      
+      /*handle first round of bets
+    
+      t.handleDecision(currentBetter Decision):
+         if fold: 
+            remove player, move currentBetter forward
+         if call:
+            set player's money-in to be t.highest-bet
+            take t.highest_bet - moneyIn from player
+            move current Better Forward
+         if raise: 
+            * must be certain amount
+            * take take t.highest_bet - moneyIn from player
+            * set highest Bet to player's new bet amount
+            * move currentBetter Forward
+            
+         
+
+  
+      */
       
       
       
